e87fd8212a2ab67930045e1ff252802375e8bb5c
leonamlucius <leonam253@gmail.com>
2025-03-26 20:46:56 -0300

    Downloads pdf working

--- DowloadPDF.py
+++ DowloadPDF.py
@@ -1,6 +1,8 @@
 import requests
 from requests.auth import HTTPBasicAuth
 from bs4 import BeautifulSoup
+import os
+from urllib.parse import urljoin
 
 
 def scrape():
@@ -11,23 +13,32 @@ def scrape():
 
     content = BeautifulSoup(response.text, 'html.parser')
 
-    all_urls = content.findAll('a')
+    all_urls = content.find_all('a')
 
-    for url in all_urls:
+    for url_tag in all_urls:
 
         try:
-            if 'pdf' in url['href'] :
+            if 'pdf' in url_tag['href'] :
 
-                if 'Anexo' in url['href']:
-                    
-                    print(url['href'])
-        except:
-            pass
+                if 'Anexo' in url_tag['href']:
+
+                    print(url_tag['href'])
+
+                    pdf_url = urljoin(url, url_tag['href'])
+
+                    pdf_reponse = requests.get(pdf_url)
+
+                    filename = pdf_url.split('/')[-1]
+
+                    cwd = os.getcwd()
+
+                    with open(os.path.join(cwd, filename), 'wb') as f:
+
+                        f.write(pdf_reponse.content)
 
 
-
-
-
+        except Exception as e:
+            print('ERRO:', e)
 
 if __name__ == "__main__":
 
